6694aaa5769a4a63ccc3c9fb1a09ef9e74d1bd66
Emilaine Figueiredo <emilaine-alves@hotmail.com>
2025-09-22 15:02:30 +0100

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-purple-700">
+      <h1 className="text-8xl font-extrabold text-white">
+        BYTES<span className="text-teal-500">4</span>FUTURE
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
